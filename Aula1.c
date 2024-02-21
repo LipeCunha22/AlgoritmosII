269570720fb368070be1f59c2859dc26bcea5d9c
LipeCunha22 <160797344+LipeCunha22@users.noreply.github.com>
2024-02-21 20:33:57 -0300

    Create Aula1.c

---
+++ Aula1.c
@@ -0,0 +1,10 @@
+//biblioteca necessaria para entrada e saida
+#include <stdio.h>
+#include <stdlib.h>
+
+//Chamada principal
+int main (){
+  printf("curso de CC\n");
+  printf("Algoritmos e program 2\n");
+  return 0;
+}
