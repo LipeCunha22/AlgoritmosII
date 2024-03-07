1b87694d80283954d2b4089188217bfc7ca3a530
LipeCunha22 <160797344+LipeCunha22@users.noreply.github.com>
2024-03-07 19:54:14 -0300

    EX1SEMANA2

--- Aula1.c
+++ Aula1.c
@@ -1,10 +1,21 @@
-//biblioteca necessaria para entrada e saida
+
+//EXERCICIO 1
 #include <stdio.h>
 #include <stdlib.h>
 
-//Chamada principal
-int main (){
-  printf("curso de CC\n");
-  printf("Algoritmos e program 2\n");
+int  main() {
+  int a, b;
+
+  printf("valor de a: ");
+  scanf("%d", &a);
+
+  printf("valor de b: ");
+  scanf("%d", &b);
+
+  printf("\na soma dos valores eh: %d", a + b);
+  printf("\na subtracao dos valores eh: %d", a - b);
+  printf("\na multiplicacao dos valores eh: %d", a * b);
+  printf("\na divisao dos valores eh: %d", a / b);
+
   return 0;
 }
